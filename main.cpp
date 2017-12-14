7eee1c7e97037d56ba149d6213aadb55dc121313
Nicolas SCHOEMAEKER <nicolas.schoemaeker@partnering.fr>
2017-12-14 10:10:36 +0100

    Set the main window's size to the screen's size

--- main.cpp
+++ main.cpp
@@ -4,14 +4,26 @@
 #include <QApplication>
 #include <QWindow>
 #include <QIcon>
+#include <QScreen>
+#include <iostream>
 
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
 
+    // Query the screen's (available) size to set the main window's size
+    QScreen *screen = QGuiApplication::primaryScreen();
+    if (screen == NULL) {
+        std::cerr << "No screen detected!" << std::endl;
+        return -1;
+    }
+
+    QRect availableGeometry = screen->availableGeometry();
+
     PapyrusWindow mainWindow;
     mainWindow.setWindowTitle(APP_NAME);
     mainWindow.setWindowIcon(QIcon(":/icons/icons/papyrus.svg"));
+    mainWindow.setGeometry(0, 0, availableGeometry.width(), availableGeometry.height());
     mainWindow.show();
 
     return app.exec();

051fa05ac25171a5813148fd4a79ab0fc3770a7e
nschoe <ns.schoe@gmail.com>
2018-07-10 17:41:48 +0200

    Add cmake dependency in dev environement

--- shell.nix
+++ shell.nix
@@ -1,5 +1,5 @@
 with import <nixpkgs> {};
 stdenv.mkDerivation {
 	name = "papyrus-dev-environment";
-  buildInputs = [ pkgconfig cryptopp qt5.full qtcreator gdb ];
+  buildInputs = [ pkgconfig cryptopp qt5.full qtcreator gdb cmake ];
 }

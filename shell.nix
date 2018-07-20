2674aa5aeb5b08059673100ab87a2cb982122d99
nschoe <ns.schoe@gmail.com>
2018-07-20 16:49:38 +0200

    Update nixOS shell.nix to use both Qt and ROS environment

--- shell.nix
+++ shell.nix
@@ -1,5 +1,16 @@
-with import <nixpkgs> {};
-stdenv.mkDerivation {
-	name = "papyrus-dev-environment";
-  buildInputs = [ pkgconfig cryptopp qt5.full qtcreator gdb cmake ];
+let pkgs1 = import /home/nschoe/nixpkgs {};
+    pkgs2 = import /home/nschoe/airapkgs {};
+in
+  pkgs1.stdenv.mkDerivation {
+    name = "papyrus-dev-env";
+    buildInputs = [ pkgs1.pkgconfig pkgs1.cryptopp pkgs1.qt5.qtbase pkgs1.qt5.qtsvg pkgs1.cmake pkgs2.ros_comm  ];
 }
+
+
+
+
+# stdenv.mkDerivation {
+	# name = "papyrus-dev-environment";
+  # buildInputs = [ pkgconfig cryptopp qt5.qtbase qt5.qtsvg cmake ];
+  # buildInputs = [ pkgconfig cryptopp qt5.full cmake ];
+# }

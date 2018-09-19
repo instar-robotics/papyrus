fdd8938b4cdcbf24a68c7ed9cfae23aead02e1d0
nschoe <ns.schoe@gmail.com>
2018-09-19 12:15:28 +0200

    Display strings on links (and change spaces to tabs...) (fix #72)

--- src/link.cpp
+++ src/link.cpp
@@ -19,133 +19,142 @@ Link::Link(OutputSlot *f, InputSlot *t, QGraphicsItem *parent) : QGraphicsItem(p
                                     m_isInvalid(false),
                                     m_connectivity(ONE_TO_ONE) // chose one by default
 {
-    m_uuid = QUuid::createUuid();
+	m_uuid = QUuid::createUuid();
 
-    // Add ourselves as input and output to the corresponding slots
-    if (f != NULL)
-        f->addOutput(this);
-    if (t != NULL)
-        t->addInput(this);
+	// Add ourselves as input and output to the corresponding slots
+	if (f != NULL)
+		f->addOutput(this);
+	if (t != NULL)
+		t->addInput(this);
 
-    setAcceptHoverEvents(true);
-    setFlag(QGraphicsItem::ItemIsSelectable, true);
+	setAcceptHoverEvents(true);
+	setFlag(QGraphicsItem::ItemIsSelectable, true);
 }
 
 QRectF Link::boundingRect() const
 {
-    // If the line is not self-looping, then returns the bounding rect of the main line
-    if (!m_selfLoop) {
-        return m_line.boundingRect();
-    } else {
-        // Otherwise, return the intersection of the three line's bounding rects
-        return m_leftSegment.boundingRect().united(m_line.boundingRect()).united(m_rightSegment.boundingRect());
-    }
+	// If the line is not self-looping, then returns the bounding rect of the main line
+	if (!m_selfLoop) {
+		return m_line.boundingRect();
+	} else {
+		// Otherwise, return the intersection of the three line's bounding rects
+		return m_leftSegment.boundingRect().united(m_line.boundingRect()).united(m_rightSegment.boundingRect());
+	}
 }
 
 void Link::paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget)
 {
-    QColor color(48, 140, 198);
-    QPen pen(color);
-    qreal width = pen.widthF();
+	QColor color(48, 140, 198);
+	QPen pen(color);
+	qreal width = pen.widthF();
 
-    if (option->state & QStyle::State_MouseOver) {
-        width += 1;
-    }
+	if (option->state & QStyle::State_MouseOver) {
+		width += 1;
+	}
 
-    // If the link is selected, set the bold value (prevent changing it when hovering)
-    if (option->state & QStyle::State_Selected) {
-        width = 4;
-    }
+	// If the link is selected, set the bold value (prevent changing it when hovering)
+	if (option->state & QStyle::State_Selected) {
+		width = 4;
+	}
 
-    // If the link is invalid, set the bold value and the color
-    if (m_isInvalid) {
-        width = 5;
-        pen.setColor(Qt::red);
-    }
+	// If the link is invalid, set the bold value and the color
+	if (m_isInvalid) {
+		width = 5;
+		pen.setColor(Qt::red);
+	}
 
-    pen.setWidthF(width);
+	pen.setWidthF(width);
 
-    // Create a copy of option and remove the State_Selected option to prevent displaying an ugly
-    // dotted rectangle
-    QStyleOptionGraphicsItem newOption(*option);
-    newOption.state.setFlag(QStyle::State_Selected, false);
+	// Create a copy of option and remove the State_Selected option to prevent displaying an ugly
+	// dotted rectangle
+	QStyleOptionGraphicsItem newOption(*option);
+	newOption.state.setFlag(QStyle::State_Selected, false);
 
-    if (m_selfLoop || m_secondary)
-        pen.setStyle(Qt::DashLine);
+	if (m_selfLoop || m_secondary)
+		pen.setStyle(Qt::DashLine);
 
-    if (!m_selfLoop) {
-        m_line.setPen(pen);
+	if (!m_selfLoop) {
+		m_line.setPen(pen);
 
-        m_line.paint(painter, &newOption, widget);
+		m_line.paint(painter, &newOption, widget);
 
-        // Paint the weight
-        if (!isStringLink() && (m_to->inputType() == SCALAR_SCALAR || m_to->inputType() == SCALAR_MATRIX)) {
-            QPen currPen = painter->pen();
+		// Paint the weight
+//        if (!isStringLink() && (m_to->inputType() == SCALAR_SCALAR || m_to->inputType() == SCALAR_MATRIX)) {
+		if (isStringLink() || m_to->inputType() == SCALAR_SCALAR || m_to->inputType() == SCALAR_MATRIX) {
+			QPen currPen = painter->pen();
 
-            // Paint the weight a different color when it's negative
-            if (m_weight < 0) {
-                painter->setPen(QColor(198, 65, 242));
-            }
+			// Paint the weight a different color when it's negative
+			if (!isStringLink() && m_weight < 0) {
+				painter->setPen(QColor(198, 65, 242));
+			}
 
-            // Compute normal vector to slightly translate the line's bounding rect so that the
-            // weight is not written *on* the line (less readable), but slightly above it
-            QLineF nV = m_line.line().normalVector();
-            nV.translate(mapToScene((m_line.line().p2() - m_line.line().p1()) / 2));
+			// Compute normal vector to slightly translate the line's bounding rect so that the
+			// weight is not written *on* the line (less readable), but slightly above it
+			QLineF nV = m_line.line().normalVector();
+			nV.translate(mapToScene((m_line.line().p2() - m_line.line().p1()) / 2));
 
-            QRectF offsetRect = boundingRect().translated((nV.p2() - nV.p1()) / 20);
-            painter->drawText(offsetRect, Qt::AlignCenter | Qt::TextDontClip, QString::number(m_weight));
+			QRectF offsetRect = boundingRect().translated((nV.p2() - nV.p1()) / 20);
 
-            // Restore the previous brush
-            if (m_weight < 0)
-                painter->setPen(currPen);
-        }
-    } else {
-        m_line.setPen(pen);
-        m_leftSegment.setPen(pen);
-        m_rightSegment.setPen(pen);
+			if (!isStringLink()) {
+				painter->drawText(offsetRect, Qt::AlignCenter | Qt::TextDontClip, QString::number(m_weight));
 
-        m_leftSegment.paint(painter, &newOption, widget);
-        m_line.paint(painter, &newOption, widget);
-        m_rightSegment.paint(painter, &newOption, widget);
+				// Restore the previous brush
+				if (m_weight < 0)
+					painter->setPen(currPen);
+			} else {
+				painter->drawText(offsetRect, Qt::AlignCenter | Qt::TextDontClip, m_value);
+			}
+		}
+	} else {
+		m_line.setPen(pen);
+		m_leftSegment.setPen(pen);
+		m_rightSegment.setPen(pen);
 
-        QPen currPen = painter->pen();
+		m_leftSegment.paint(painter, &newOption, widget);
+		m_line.paint(painter, &newOption, widget);
+		m_rightSegment.paint(painter, &newOption, widget);
 
-        // Paint the weight a different color when it's negative
-        if (m_weight < 0) {
-            painter->setPen(QColor(198, 65, 242));
-        }
+		QPen currPen = painter->pen();
 
-        // Paint the weight
-        if (!isStringLink() && (m_to->inputType() == SCALAR_SCALAR || m_to->inputType() == SCALAR_MATRIX)) {
-            QRectF r = m_line.boundingRect();
-            r.setTop(r.top() - 30);
-            painter->drawText(r, Qt::AlignCenter, QString::number(m_weight));
+		// Paint the weight a different color when it's negative
+		if (!isStringLink() && m_weight < 0) {
+			painter->setPen(QColor(198, 65, 242));
+		}
 
-        }
+		// Paint the weight
+		if (isStringLink() || m_to->inputType() == SCALAR_SCALAR || m_to->inputType() == SCALAR_MATRIX) {
+			QRectF r = m_line.boundingRect();
+			r.setTop(r.top() - 30);
 
-        // Restore the previous brush
-        if (m_weight < 0)
-            painter->setPen(currPen);
-    }
+			if (!isStringLink())
+				painter->drawText(r, Qt::AlignCenter, QString::number(m_weight));
+			else
+				painter->drawText(r, Qt::AlignCenter, m_value);
+		}
+
+		// Restore the previous brush
+		if (!isStringLink() && m_weight < 0)
+			painter->setPen(currPen);
+	}
 }
 
 QPainterPath Link::shape() const
 {
-    if (!m_selfLoop) {
-        QPainterPathStroker *stroke = new QPainterPathStroker();
-        stroke->setWidth(60.0);
-        return stroke->createStroke(m_line.shape());
-    } else {
-        // This is not perfect, for some reason the m_line path is not symmetric
-        QPainterPath path(m_rightSegment.shape());
-        path.addPath(m_line.shape());
-        path.addPath(m_leftSegment.shape());
+	if (!m_selfLoop) {
+		QPainterPathStroker *stroke = new QPainterPathStroker();
+		stroke->setWidth(60.0);
+		return stroke->createStroke(m_line.shape());
+	} else {
+		// This is not perfect, for some reason the m_line path is not symmetric
+		QPainterPath path(m_rightSegment.shape());
+		path.addPath(m_line.shape());
+		path.addPath(m_leftSegment.shape());
 
-        QPainterPathStroker *stroke = new QPainterPathStroker();
-        stroke->setWidth(20.0);
-        stroke->setJoinStyle(Qt::RoundJoin);
-        return stroke->createStroke(path);
-    }
+		QPainterPathStroker *stroke = new QPainterPathStroker();
+		stroke->setWidth(20.0);
+		stroke->setJoinStyle(Qt::RoundJoin);
+		return stroke->createStroke(path);
+	}
 }
 
 /**
@@ -156,11 +165,11 @@ QPainterPath Link::shape() const
  */
 bool Link::isStringLink()
 {
-    if (m_from == NULL || m_to == NULL)
-        informUserAndCrash(tr("Trying to check whether a Link is a string link, but it is missing "
-                              "either its from or to box!"));
+	if (m_from == NULL || m_to == NULL)
+		informUserAndCrash(tr("Trying to check whether a Link is a string link, but it is missing "
+		                      "either its from or to box!"));
 
-    return m_from->outputType() == STRING && m_to->inputType() == STRING_INPUT;
+	return m_from->outputType() == STRING && m_to->inputType() == STRING_INPUT;
 }
 
 /**
@@ -171,160 +180,160 @@ bool Link::isStringLink()
  */
 void Link::addLinesToScene()
 {
-    DiagramScene *dscene = dynamic_cast<DiagramScene *>(scene());
-    if (dscene == NULL)
-        informUserAndCrash("Could not cast scene in DiagramScene");
+	DiagramScene *dscene = dynamic_cast<DiagramScene *>(scene());
+	if (dscene == NULL)
+		informUserAndCrash("Could not cast scene in DiagramScene");
 
-    if (!m_selfLoop) {
-        dscene->addItem(&m_line);
-    } else {
-        dscene->addItem(&m_leftSegment);
-        dscene->addItem(&m_line);
-        dscene->addItem(&m_rightSegment);
-    }
+	if (!m_selfLoop) {
+		dscene->addItem(&m_line);
+	} else {
+		dscene->addItem(&m_leftSegment);
+		dscene->addItem(&m_line);
+		dscene->addItem(&m_rightSegment);
+	}
 
-    updateLines();
+	updateLines();
 }
 
 QUuid Link::uuid() const
 {
-    return m_uuid;
+	return m_uuid;
 }
 
 void Link::setUuid(const QUuid &uuid)
 {
-    m_uuid = uuid;
+	m_uuid = uuid;
 }
 
 OutputSlot *Link::from() const
 {
-    return m_from;
+	return m_from;
 }
 
 void Link::setFrom(OutputSlot *from)
 {
-    m_from = from;
-    m_selfLoop = checkIfSelfLoop();
-    updateLines();
+	m_from = from;
+	m_selfLoop = checkIfSelfLoop();
+	updateLines();
 }
 
 InputSlot *Link::to() const
 {
-    return m_to;
+	return m_to;
 }
 
 void Link::setTo(InputSlot *to)
 {
-    m_to = to;
-    m_selfLoop = checkIfSelfLoop();
-    updateLines();
+	m_to = to;
+	m_selfLoop = checkIfSelfLoop();
+	updateLines();
 }
 
 bool Link::secondary() const
 {
-    return m_secondary;
+	return m_secondary;
 }
 
 void Link::setSecondary(bool secondary)
 {
-    m_secondary = secondary;
+	m_secondary = secondary;
 }
 
 bool Link::checkIfSelfLoop()
 {
-    // If either 'from' or 'to' is NULL, then it cannot be a secondary link
-    if (m_from == NULL || m_to == NULL) {
-        return false;
-    }
-    // then check if some slots are not associated
-    else if (m_from->box() == NULL || m_to->box() == NULL) {
-        return false;
-    }
-    // otherwise, check if the associated boxes for 'from' and 'to' are the same
-    else if (m_from->box() == m_to->box()) {
-        return true;
-    }
-    else {
-        return false;
-    }
+	// If either 'from' or 'to' is NULL, then it cannot be a secondary link
+	if (m_from == NULL || m_to == NULL) {
+		return false;
+	}
+	// then check if some slots are not associated
+	else if (m_from->box() == NULL || m_to->box() == NULL) {
+		return false;
+	}
+	// otherwise, check if the associated boxes for 'from' and 'to' are the same
+	else if (m_from->box() == m_to->box()) {
+		return true;
+	}
+	else {
+		return false;
+	}
 }
 
 Connectivity Link::connectivity() const
 {
-    return m_connectivity;
+	return m_connectivity;
 }
 
 void Link::setConnectivity(const Connectivity &connectivity)
 {
-    m_connectivity = connectivity;
+	m_connectivity = connectivity;
 }
 
 QString Link::value() const
 {
-    return m_value;
+	return m_value;
 }
 
 void Link::setValue(const QString &value)
 {
-    m_value = value;
+	m_value = value;
 }
 
 bool Link::selfLoop() const
 {
-    return m_selfLoop;
+	return m_selfLoop;
 }
 
 bool Link::isInvalid() const
 {
-    return m_isInvalid;
+	return m_isInvalid;
 }
 
 void Link::setIsInvalid(bool isInvalid)
 {
-    m_isInvalid = isInvalid;
+	m_isInvalid = isInvalid;
 }
 
 qreal Link::weight() const
 {
-    return m_weight;
+	return m_weight;
 }
 
 void Link::setWeight(const qreal &weight)
 {
-    m_weight = weight;
+	m_weight = weight;
 }
 
 void Link::updateLines()
 {
-    // Don't do anything if either 'from' or 'to' is null
-    if (m_from == NULL || m_to == NULL)
-        return;
+	// Don't do anything if either 'from' or 'to' is null
+	if (m_from == NULL || m_to == NULL)
+		return;
 
-    QPointF orig = mapFromItem(m_from, m_from->boundingRect().center());
-    QPointF end  = mapFromItem(m_to, m_to->boundingRect().center());
+	QPointF orig = mapFromItem(m_from, m_from->boundingRect().center());
+	QPointF end  = mapFromItem(m_to, m_to->boundingRect().center());
 
-    // Create just a single line when not secondary link
-    if (!m_selfLoop) {
-        m_line.setLine(QLineF(orig, end));
-    } else {
-        // When in secondary, we have to create two vertical lines
-        QPointF one = orig;
-        QPointF two = end;
+	// Create just a single line when not secondary link
+	if (!m_selfLoop) {
+		m_line.setLine(QLineF(orig, end));
+	} else {
+		// When in secondary, we have to create two vertical lines
+		QPointF one = orig;
+		QPointF two = end;
 
-        // We need a way to differentiate multiple self-links on the same function box
-        // this is why we use the yDiff to offset the values for the segments
-        qreal yDiff = one.y() - two.y();
+		// We need a way to differentiate multiple self-links on the same function box
+		// this is why we use the yDiff to offset the values for the segments
+		qreal yDiff = one.y() - two.y();
 
-        one.ry() -= 75 + yDiff;
-        two.setY(one.y());
+		one.ry() -= 75 + yDiff;
+		two.setY(one.y());
 
-        one.rx() -= qAbs(yDiff);
-        two.rx() += qAbs(yDiff);
+		one.rx() -= qAbs(yDiff);
+		two.rx() += qAbs(yDiff);
 
-        m_leftSegment.setLine(QLineF(two, end));
-        m_rightSegment.setLine(QLineF(orig, one));
-        m_line.setLine(QLineF(one, two));
-    }
+		m_leftSegment.setLine(QLineF(two, end));
+		m_rightSegment.setLine(QLineF(orig, one));
+		m_line.setLine(QLineF(one, two));
+	}
 }
 
 /**
@@ -333,38 +342,38 @@ void Link::updateLines()
  */
 bool Link::checkIfInvalid()
 {
-    // Pointers check
-    if (m_to == NULL)
-        informUserAndCrash(tr("Can't check if Link is invalid: no InputSlot"));
+	// Pointers check
+	if (m_to == NULL)
+		informUserAndCrash(tr("Can't check if Link is invalid: no InputSlot"));
 
-    if (m_from == NULL)
-        informUserAndCrash(tr("Can't check if Link is invalid: no OutputSlot"));
+	if (m_from == NULL)
+		informUserAndCrash(tr("Can't check if Link is invalid: no OutputSlot"));
 
-    if (m_to->inputType() == SCALAR_MATRIX) {
-                DiagramBox *box = m_to->box();
-        if (box == NULL) {
-            qDebug() << "\t\tdestination box is null";
-            informUserAndCrash(tr("Link with UUID %1 cannot be checked for invalidity: it has no "
-                                  "destination box").arg(m_uuid.toString()));
-        }
+	if (m_to->inputType() == SCALAR_MATRIX) {
+		        DiagramBox *box = m_to->box();
+				if (box == NULL) {
+			qDebug() << "\t\tdestination box is null";
+			informUserAndCrash(tr("Link with UUID %1 cannot be checked for invalidity: it has no "
+			                      "destination box").arg(m_uuid.toString()));
+		}
 
-        int toRows = box->rows();
-        int toCols = box->cols();
+		int toRows = box->rows();
+		int toCols = box->cols();
 
-        box = m_from->box();
-        if (box == NULL)
-            informUserAndCrash(tr("Can't check if Link is invalid: no origin box"));
-        int fromRows = box->rows();
-        int fromCols = box->cols();
+		box = m_from->box();
+		if (box == NULL)
+			informUserAndCrash(tr("Can't check if Link is invalid: no origin box"));
+		int fromRows = box->rows();
+		int fromCols = box->cols();
 
-        // I *know* parentheses are optional here, but they increase readability, so sue me
-        m_isInvalid = (toRows != fromRows) || (toCols != fromCols);
-    } else {
-        // This is considered valid
-        m_isInvalid = false;
-    }
+		// I *know* parentheses are optional here, but they increase readability, so sue me
+		m_isInvalid = (toRows != fromRows) || (toCols != fromCols);
+	} else {
+		// This is considered valid
+		m_isInvalid = false;
+	}
 
-    // At the end, return wether we are invalid
-    return m_isInvalid;
+	// At the end, return wether we are invalid
+	return m_isInvalid;
 }
 

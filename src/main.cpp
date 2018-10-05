e13bb9fb47221ad1889a5a45f0703a5f70f8dc71
nschoe <ns.schoe@gmail.com>
2018-10-05 14:31:54 +0200

    Replace spaces by tabs

--- src/main.cpp
+++ src/main.cpp
@@ -10,15 +10,15 @@
 
 int main(int argc, char *argv[])
 {
-    qInstallMessageHandler(coloredMessageHandler);
-    QApplication app(argc, argv);
+	qInstallMessageHandler(coloredMessageHandler);
+	QApplication app(argc, argv);
 
-    ros::init(argc, argv, "Papyrus");
+	ros::init(argc, argv, "Papyrus");
 
-    PapyrusWindow mainWindow(argc, argv);
-    mainWindow.setWindowTitle(APP_NAME);
-    mainWindow.setWindowIcon(QIcon(":/icons/icons/papyrus.svg"));
-    mainWindow.show();
+	PapyrusWindow mainWindow(argc, argv);
+	mainWindow.setWindowTitle(APP_NAME);
+	mainWindow.setWindowIcon(QIcon(":/icons/icons/papyrus.svg"));
+	mainWindow.show();
 
-    return app.exec();
+	return app.exec();
 }

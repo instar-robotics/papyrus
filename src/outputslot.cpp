2c4811f709a1e9744b9b569e9153b01d0a4ed98c
nschoe <ns.schoe@gmail.com>
2018-10-10 10:40:29 +0200

    Change spaces to tabs

--- src/outputslot.cpp
+++ src/outputslot.cpp
@@ -21,23 +21,23 @@ OutputSlot::OutputSlot() : Slot(),
 
 std::set<Link *> OutputSlot::outputs() const
 {
-    return m_outputs;
+	return m_outputs;
 }
 
 void OutputSlot::addOutput(Link *output)
 {
-    if (output == NULL)
-        return;
+	if (output == NULL)
+		return;
 
-    m_outputs.insert(output);
+	m_outputs.insert(output);
 }
 
 void OutputSlot::removeOutput(Link *output)
 {
-    if (output == NULL)
-        return;
+	if (output == NULL)
+		return;
 
-    m_outputs.erase(output);
+	m_outputs.erase(output);
 }
 
 /**
@@ -49,63 +49,64 @@ void OutputSlot::removeOutput(Link *output)
  */
 void OutputSlot::paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget)
 {
-    Q_UNUSED(widget);
+	Q_UNUSED(widget);
 
-    QPen pen;
-    qreal width = 1.5;
+	QPen pen;
+	qreal width = 1.5;
 //    QFont font = painter->font();
 
-    qreal cx = 0;
-    qreal cy = 0;
-    qreal rx = 5;
-    qreal ry = 5;
+	qreal cx = 0;
+	qreal cy = 0;
+	qreal rx = 5;
+	qreal ry = 5;
 
-    if (option->state & QStyle::State_MouseOver) {
-        width += 1;
-    }
+	if (option->state & QStyle::State_MouseOver) {
+		width += 1;
+	}
 
-    QGraphicsScene *scene_ = scene();
-    DiagramScene *dscene = dynamic_cast<DiagramScene *>(scene_);
+	QGraphicsScene *scene_ = scene();
+	DiagramScene *dscene = dynamic_cast<DiagramScene *>(scene_);
 
-    if (dscene == NULL) {
-        qFatal("Could not cast the scene into a DiagramScene!");
-    }
+	if (dscene == NULL) {
+		qFatal("Could not cast the scene into a DiagramScene!");
+	}
 
-    // Change the output slot's size only if not drawing a line
-    if (dscene->line() == NULL) {
-        // Make the slot bigger when the mouse is near it
-        qreal sizeOffset = (400 - m_dist) / 100; // Grows linearly with distance -> quadratic should be better
+	// Change the output slot's size only if not drawing a line
+	if (dscene->line() == NULL) {
+		// Make the slot bigger when the mouse is near it
+		qreal sizeOffset = (400 - m_dist) / 100; // Grows linearly with distance -> quadratic should be better
 
-        rx += pow(sizeOffset, 2) / 6;
-        ry += pow(sizeOffset, 2) / 6;
-    }
+		rx += pow(sizeOffset, 2) / 6;
+		ry += pow(sizeOffset, 2) / 6;
+	}
 
-    // Subtract the half the line's width to prevetn drawing outside the boudingRect
-    rx -= width / 2.0;
-    ry -= width / 2.0;
+	// Subtract the half the line's width to prevetn drawing outside the boudingRect
+	rx -= width / 2.0;
+	ry -= width / 2.0;
 
-    pen.setWidth(width);
-    painter->setPen(pen);
+	pen.setWidth(width);
+	painter->setPen(pen);
 
-    painter->drawEllipse(QPointF(cx, cy), rx, ry);
+	painter->drawEllipse(QPointF(cx, cy), rx, ry);
 }
 
 QRectF OutputSlot::boundingRect() const
 {
-    // Set to be the same size as the input slot, for arbitrary reason
-    return QRectF(-7.7, -7.7, 15.4, 15.4);
+	// Set to be the same size as the input slot, for arbitrary reason
+	return QRectF(-7.7, -7.7, 15.4, 15.4);
 }
 
 /**
  * @brief OutputSlot::mousePressEvent disable the RubberBang drag mode for the scene during the
- * creation of the link
+ * creation of the link and prevents propagating the click so that we can create a link even when
+ * the box is inside a comment Zone
  * @param evt
  */
 void OutputSlot::mousePressEvent(QGraphicsSceneMouseEvent *evt)
 {
-    m_isDrawingLine = true;
+	m_isDrawingLine = true;
 
-    QGraphicsItem::mousePressEvent(evt);
+	QGraphicsItem::mousePressEvent(evt);
 }
 
 /**
@@ -116,18 +117,18 @@ void OutputSlot::mousePressEvent(QGraphicsSceneMouseEvent *evt)
  */
 void OutputSlot::hoverEnterEvent(QGraphicsSceneHoverEvent *evt)
 {
-    QList<QGraphicsView *> views = scene()->views();
+	QList<QGraphicsView *> views = scene()->views();
 
-    foreach (QGraphicsView *view, views) {
-        // Change the view's drag mode to NoDrag only if it's a DiagramView (to prevent messing with
-        // other views, like the minimap, etc.)
-        DiagramView *view_ = dynamic_cast<DiagramView *>(view);
-        if (view_ != NULL) {
-            view_->setDragMode(QGraphicsView::NoDrag);
-        }
-    }
+	foreach (QGraphicsView *view, views) {
+		// Change the view's drag mode to NoDrag only if it's a DiagramView (to prevent messing with
+		// other views, like the minimap, etc.)
+		DiagramView *view_ = dynamic_cast<DiagramView *>(view);
+		if (view_ != NULL) {
+			view_->setDragMode(QGraphicsView::NoDrag);
+		}
+	}
 
-    Slot::hoverEnterEvent(evt);
+	Slot::hoverEnterEvent(evt);
 }
 
 /**
@@ -137,28 +138,28 @@ void OutputSlot::hoverEnterEvent(QGraphicsSceneHoverEvent *evt)
  */
 void OutputSlot::hoverLeaveEvent(QGraphicsSceneHoverEvent *evt)
 {
-    QList<QGraphicsView *> views = scene()->views();
+	QList<QGraphicsView *> views = scene()->views();
 
-    foreach (QGraphicsView *view, views) {
-        // Change the view's drag mode to rubber band only if it's a DiagramView (to prevent messing
-        // with other views, like the minimap, etc.)
-        DiagramView *view_ = dynamic_cast<DiagramView *>(view);
-        if (view_ != NULL) {
-            view_->setDragMode(QGraphicsView::RubberBandDrag);
-        }
-    }
+	foreach (QGraphicsView *view, views) {
+		// Change the view's drag mode to rubber band only if it's a DiagramView (to prevent messing
+		// with other views, like the minimap, etc.)
+		DiagramView *view_ = dynamic_cast<DiagramView *>(view);
+		if (view_ != NULL) {
+			view_->setDragMode(QGraphicsView::RubberBandDrag);
+		}
+	}
 
-    Slot::hoverLeaveEvent(evt);
+	Slot::hoverLeaveEvent(evt);
 }
 
 bool OutputSlot::isDrawingLine() const
 {
-    return m_isDrawingLine;
+	return m_isDrawingLine;
 }
 
 void OutputSlot::setIsDrawingLine(bool isDrawingLine)
 {
-    m_isDrawingLine = isDrawingLine;
+	m_isDrawingLine = isDrawingLine;
 }
 
 /**
@@ -167,18 +168,18 @@ void OutputSlot::setIsDrawingLine(bool isDrawingLine)
  */
 void OutputSlot::updateLinks()
 {
-    foreach(Link *link, m_outputs) {
-        link->updateLines();
-        link->update();
-    }
+	foreach(Link *link, m_outputs) {
+		link->updateLines();
+		link->update();
+	}
 }
 
 OutputType OutputSlot::outputType() const
 {
-    return m_outputType;
+	return m_outputType;
 }
 
 void OutputSlot::setOutputType(const OutputType &outputType)
 {
-    m_outputType = outputType;
+	m_outputType = outputType;
 }

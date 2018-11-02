08282aad62120c533cd774bd030b99e87b1cdb5e
nschoe <ns.schoe@gmail.com>
2018-11-02 12:55:32 +0100

    Make 'multiple' attribute optional, an defaults to false (fix #107) (sorry tabs vs. spaces too)

--- src/xmldescriptionreader.cpp
+++ src/xmldescriptionreader.cpp
@@ -20,119 +20,119 @@ XmlDescriptionReader::XmlDescriptionReader(Category* category) : m_category(cate
  */
 bool XmlDescriptionReader::read(QIODevice *device, const QString &descriptionFile)
 {
-    reader.setDevice(device);
+	reader.setDevice(device);
 
-    if (reader.readNextStartElement()) {
-        if (reader.name() == XML_ROOT_ELEM) {
-            readDescription(descriptionFile);
-        } else {
-            reader.raiseError(QObject::tr("Invalid description file"));
-        }
-    }
+	if (reader.readNextStartElement()) {
+		if (reader.name() == XML_ROOT_ELEM) {
+			readDescription(descriptionFile);
+		} else {
+			reader.raiseError(QObject::tr("Invalid description file"));
+		}
+	}
 
-    return !reader.error();
+	return !reader.error();
 }
 
 void XmlDescriptionReader::readDescription(const QString &descriptionFile)
 {
-    Q_ASSERT(reader.isStartElement() && reader.name() == XML_ROOT_ELEM);
+	Q_ASSERT(reader.isStartElement() && reader.name() == XML_ROOT_ELEM);
 
-    if (!reader.readNextStartElement()) {
-        reader.raiseError(QObject::tr("Empty description"));
-        qWarning() << "Empty description";
-        return;
-    }
+	if (!reader.readNextStartElement()) {
+		reader.raiseError(QObject::tr("Empty description"));
+		qWarning() << "Empty description";
+		return;
+	}
 
-    // Read the <libname>
-    if (reader.name() != "libname") {
-        reader.raiseError(QObject::tr("Missing <libname> tag"));
-        qWarning() << "Missing <libname> tag";
-        return;
-    }
+	// Read the <libname>
+	if (reader.name() != "libname") {
+		reader.raiseError(QObject::tr("Missing <libname> tag"));
+		qWarning() << "Missing <libname> tag";
+		return;
+	}
 
-    QString libName = reader.readElementText();
+	QString libName = reader.readElementText();
 
-    // Read the <functions>
-    if (!reader.readNextStartElement() || reader.name() != "functions") {
-        reader.raiseError(QObject::tr("Missing <functions> tag"));
-        qWarning() << "Missing <functions> tag";
-        return;
-    }
+	// Read the <functions>
+	if (!reader.readNextStartElement() || reader.name() != "functions") {
+		reader.raiseError(QObject::tr("Missing <functions> tag"));
+		qWarning() << "Missing <functions> tag";
+		return;
+	}
 
-    readAllFunctions(libName, descriptionFile);
+	readAllFunctions(libName, descriptionFile);
 }
 
 void XmlDescriptionReader::readAllFunctions(const QString &libName, const QString &descriptionFile)
 {
-    Q_ASSERT(reader.isStartElement() && reader.name() == "functions");
+	Q_ASSERT(reader.isStartElement() && reader.name() == "functions");
 
-    while (reader.readNextStartElement()) {
-        if (reader.name() == "function")
-            readOneFunction(libName, descriptionFile);
-        else
-            reader.skipCurrentElement();
-    }
+	while (reader.readNextStartElement()) {
+		if (reader.name() == "function")
+			readOneFunction(libName, descriptionFile);
+		else
+			reader.skipCurrentElement();
+	}
 }
 
 void XmlDescriptionReader::readOneFunction(const QString &libName, const QString &descriptionFile)
 {
-    Q_ASSERT(reader.isStartElement() && reader.name() == "function");
+	Q_ASSERT(reader.isStartElement() && reader.name() == "function");
 
-    Function *function = new Function(descriptionFile);
-    function->setLibName(libName);
-    QString iconFilename;
+	Function *function = new Function(descriptionFile);
+	function->setLibName(libName);
+	QString iconFilename;
 
 
-    while (reader.readNextStartElement()) {
-        if (reader.name() == "name") {
-            readName(function);
-        } else if (reader.name() == "inputs") {
-            readInputs(function);
-        } else if (reader.name() == "output") {
-            readOutput(function);
-        } else if (reader.name() == "icon") {
-            iconFilename = readIcon();
-        } else {
-            qWarning() << "Skipping unsupported tag <" << reader.name() << ">";
-            reader.skipCurrentElement();
-        }
-    }
+	while (reader.readNextStartElement()) {
+		if (reader.name() == "name") {
+			readName(function);
+		} else if (reader.name() == "inputs") {
+			readInputs(function);
+		} else if (reader.name() == "output") {
+			readOutput(function);
+		} else if (reader.name() == "icon") {
+			iconFilename = readIcon();
+		} else {
+			qWarning() << "Skipping unsupported tag <" << reader.name() << ">";
+			reader.skipCurrentElement();
+		}
+	}
 
-    // After we're done parsing the function, we check if we found an icon, and if it exists,
-    // otherwise we replace it with the missnig-icon function
-    if (iconFilename.isEmpty()) {
-        function->setIcon(0, QIcon(":/icons/icons/missing-icon.svg"));
-        function->setIconFilepath(":/icons/icons/missing-icon.svg");
-    }
-    else {
-        // Get the current directory from the description file, and search an 'icons/' directory
-        QFileInfo info(descriptionFile);
-        QDir dir(info.absoluteDir());
-        QString iconsDir = dir.absoluteFilePath("icons");
+	// After we're done parsing the function, we check if we found an icon, and if it exists,
+	// otherwise we replace it with the missnig-icon function
+	if (iconFilename.isEmpty()) {
+		function->setIcon(0, QIcon(":/icons/icons/missing-icon.svg"));
+		function->setIconFilepath(":/icons/icons/missing-icon.svg");
+	}
+	else {
+		// Get the current directory from the description file, and search an 'icons/' directory
+		QFileInfo info(descriptionFile);
+		QDir dir(info.absoluteDir());
+		QString iconsDir = dir.absoluteFilePath("icons");
 
-        // Check if the designated icon exists
-        QFileInfo iconInfo(iconsDir + "/" + iconFilename);
-        if (iconInfo.exists()) {
-            function->setIcon(0, QIcon(iconsDir + "/" + iconFilename));
-            function->setIconFilepath(iconsDir + "/" + iconFilename);
-        } else {
-            qWarning() << "Missing icon " << iconInfo.absoluteFilePath() << ", setting missing icon"
-                                                                            " instead";
-            function->setIcon(0, QIcon(":/icons/icons/missing-icon.svg"));
-            function->setIconFilepath(":/icons/icons/missing-icon.svg");
-        }
-    }
+		// Check if the designated icon exists
+		QFileInfo iconInfo(iconsDir + "/" + iconFilename);
+		if (iconInfo.exists()) {
+			function->setIcon(0, QIcon(iconsDir + "/" + iconFilename));
+			function->setIconFilepath(iconsDir + "/" + iconFilename);
+		} else {
+			qWarning() << "Missing icon " << iconInfo.absoluteFilePath() << ", setting missing icon"
+			                                                                " instead";
+			function->setIcon(0, QIcon(":/icons/icons/missing-icon.svg"));
+			function->setIconFilepath(":/icons/icons/missing-icon.svg");
+		}
+	}
 
-    function->setText(0, function->name());
-    function->setSizeHint(0, QSize(LIBRARY_ICON_SIZE, LIBRARY_ICON_SIZE));
+	function->setText(0, function->name());
+	function->setSizeHint(0, QSize(LIBRARY_ICON_SIZE, LIBRARY_ICON_SIZE));
 
-    // Add a background color to functions whose output are MATRIX
-    if (function->output()->outputType() == MATRIX)
-        function->setBackground(0, QBrush(QColor(0xffeeee)));
+	// Add a background color to functions whose output are MATRIX
+	if (function->output()->outputType() == MATRIX)
+		function->setBackground(0, QBrush(QColor(0xffeeee)));
 
-    // Only add the function to the library (category) if there was no error while parsing it.
-    if (!reader.error())
-        m_category->addChild(function);
+	// Only add the function to the library (category) if there was no error while parsing it.
+	if (!reader.error())
+		m_category->addChild(function);
 }
 
 /**
@@ -141,16 +141,16 @@ void XmlDescriptionReader::readOneFunction(const QString &libName, const QString
  */
 void XmlDescriptionReader::readName(Function *function)
 {
-    Q_ASSERT(reader.isStartElement() && reader.name() == "name");
+	Q_ASSERT(reader.isStartElement() && reader.name() == "name");
 
-    QString name = reader.readElementText();
+	QString name = reader.readElementText();
 
-    if (name.isEmpty()) {
-        reader.raiseError("Empty function names are not allowed");
-        reader.skipCurrentElement();
-    }
+	if (name.isEmpty()) {
+		reader.raiseError("Empty function names are not allowed");
+		reader.skipCurrentElement();
+	}
 
-    function->setName(name);
+	function->setName(name);
 }
 
 /**
@@ -159,92 +159,91 @@ void XmlDescriptionReader::readName(Function *function)
  */
 void XmlDescriptionReader::readInputs(Function *function)
 {
-    Q_ASSERT(reader.isStartElement() && reader.name() == "inputs");
+	Q_ASSERT(reader.isStartElement() && reader.name() == "inputs");
 
-    while (reader.readNextStartElement()) {
-        if (reader.name() == "input") {
-            InputSlot *inputSlot = new InputSlot;
-            QXmlStreamAttributes attributes = reader.attributes();
+	while (reader.readNextStartElement()) {
+		if (reader.name() == "input") {
+			InputSlot *inputSlot = new InputSlot;
+			QXmlStreamAttributes attributes = reader.attributes();
 
-            // Read the 'multiple' attribute
-            if (!attributes.hasAttribute("multiple")) {
-                qWarning() << "Missing boolean attribute 'multiple'";
-                reader.raiseError(QObject::tr("Missing boolean attribute 'multiple'."));
-                continue;
-            }
+			// Read the optional 'multiple' attribute, defaults to false
+			if (attributes.hasAttribute("multiple")) {
+				if (attributes.value("multiple").toString() == "true")
+					inputSlot->setMultiple(true);
+				else if (attributes.value("multiple").toString() == "false")
+					inputSlot->setMultiple(false);
+				else {
+					qWarning() << "Invalid attribute value 'multiple'";
+					reader.raiseError(QObject::tr("Invalid attribute value 'multiple'"));
+				}
+			} else {
+				inputSlot->setMultiple(false);
+			}
 
-            if (attributes.value("multiple").toString() == "true")
-                inputSlot->setMultiple(true);
-            else if (attributes.value("multiple").toString() == "false")
-                inputSlot->setMultiple(false);
-            else {
-                qWarning() << "Invalid attribute value 'multiple'";
-                reader.raiseError(QObject::tr("Invalid attribute value 'multiple'"));
-            }
 
-            // Read the 'type' attribute
-            if (!attributes.hasAttribute("type")) {
-                qWarning() << "Missing attribute 'type'";
-                reader.raiseError(QObject::tr("Missing attribute 'type'"));
-                continue;
-            }
+			// Read the 'type' attribute
+			if (!attributes.hasAttribute("type")) {
+				qWarning() << "Missing attribute 'type'";
+				reader.raiseError(QObject::tr("Missing attribute 'type'"));
+				continue;
+			}
 
-            inputSlot->setInputType(stringToInputType(attributes.value("type").toString()));
+			inputSlot->setInputType(stringToInputType(attributes.value("type").toString()));
 
-            // Read the <name> tag
-            while (reader.readNextStartElement()) {
-                if (reader.name() == "name")
-                    readParameterName(inputSlot);
-                else
-                    reader.skipCurrentElement();
-            }
+			// Read the <name> tag
+			while (reader.readNextStartElement()) {
+				if (reader.name() == "name")
+					readParameterName(inputSlot);
+				else
+					reader.skipCurrentElement();
+			}
 
-            function->addInputSlot(inputSlot);
-        } else {
-            reader.skipCurrentElement();
-        }
-    }
+			function->addInputSlot(inputSlot);
+		} else {
+			reader.skipCurrentElement();
+		}
+	}
 }
 
 QString XmlDescriptionReader::readIcon()
 {
-    Q_ASSERT(reader.isStartElement() && reader.name() == "icon");
+	Q_ASSERT(reader.isStartElement() && reader.name() == "icon");
 
-    return reader.readElementText();
+	return reader.readElementText();
 }
 
 void XmlDescriptionReader::readParameterName(Slot *paramSlot)
 {
-    Q_ASSERT(reader.isStartElement() && reader.name() == "name");
+	Q_ASSERT(reader.isStartElement() && reader.name() == "name");
 
-    QString paramName = reader.readElementText();
+	QString paramName = reader.readElementText();
 
-    if (!paramName.isEmpty()) {
-        paramSlot->setName(paramName);
-    } else {
-        qWarning() << "Empty parameter name are not allowed";
-        reader.raiseError("Empty parameter name are not allowed");
-        reader.skipCurrentElement();
-    }
+	if (!paramName.isEmpty()) {
+		paramSlot->setName(paramName);
+	} else {
+		qWarning() << "Empty parameter name are not allowed";
+		reader.raiseError("Empty parameter name are not allowed");
+		reader.skipCurrentElement();
+	}
 }
 
 void XmlDescriptionReader::readOutput(Function *function)
 {
-    Q_ASSERT(reader.isStartElement() && reader.name() == "output");
+	Q_ASSERT(reader.isStartElement() && reader.name() == "output");
 
-    OutputSlot *outputSlot = new OutputSlot;
+	OutputSlot *outputSlot = new OutputSlot;
 
-    QXmlStreamAttributes attributes = reader.attributes();
-    if (!attributes.hasAttribute("type")) {
-        qWarning() << "<output> tag is missing its 'type' attribute";
-        reader.raiseError();
-        return;
-    }
+	QXmlStreamAttributes attributes = reader.attributes();
+	if (!attributes.hasAttribute("type")) {
+		qWarning() << "<output> tag is missing its 'type' attribute";
+		reader.raiseError();
+		return;
+	}
 
-    outputSlot->setOutputType(stringToOutputType(attributes.value("type").toString()));
-    function->setOutput(outputSlot);
+	outputSlot->setOutputType(stringToOutputType(attributes.value("type").toString()));
+	function->setOutput(outputSlot);
 
-    // Nothing to read, but it's just to consume the <output> element
-    while (reader.readNextStartElement());
+	// Nothing to read, but it's just to consume the <output> element
+	while (reader.readNextStartElement());
 }
 

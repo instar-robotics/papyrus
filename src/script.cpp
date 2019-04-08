0faeb21f82bd5bc9015f8e64fd0dca237f2dfd12
nschoe <nschoe@protonmail.com>
2019-04-08 23:05:35 +0200

    Append LICENSE to source files

--- src/script.cpp
+++ src/script.cpp
@@ -1,3 +1,24 @@
+/*
+  Copyright (C) INSTAR Robotics
+
+  Author: Nicolas SCHOEMAEKER
+ 
+  This file is part of papyrus <https://github.com/instar-robotics/papyrus>.
+ 
+  papyrus is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+ 
+  papyrus is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+ 
+  You should have received a copy of the GNU General Public License
+  along with dogtag. If not, see <http://www.gnu.org/licenses/>.
+*/
+
 ﻿#include "script.h"
 #include "papyruswindow.h"
 #include "ui_papyruswindow.h"

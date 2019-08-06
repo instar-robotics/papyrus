instar-robotics/papyrus

commit 6e86a109dab9c31841db50b74ddc5381097e4208

Set a bar at the top of a 3D visualization to move it (#44)

src/diagramscene.cpp
@@ -100,13 +100,20 @@ void DiagramScene::createOpenGLWidget(DiagramBox * box)
 {
 	if(box->getDisplayedProxy() == nullptr)
 	{
+
+
+		// Insert the widget
+
 		OpenGLMatrix *matrix = new OpenGLMatrix(100,100);
 		matrix->initializeGL();
-		OpenGLProxy *proxy = new OpenGLProxy(matrix);
+		QGraphicsRectItem *proxyMoveBar = new QGraphicsRectItem();
+		OpenGLProxy *proxy = new OpenGLProxy(matrix, proxyMoveBar);
+
+		proxy->setPos(0, proxyMoveBar->rect().height());
+		addItem(proxyMoveBar);
 		addItem(proxy);
 		box->setDisplayedProxy(proxy);
 		connect(proxy, SIGNAL(proxyDestroyed()), box, SLOT(deleteOpenGLDisplay()));
-		box->setDisplayedProxy(proxy);
 	}
 }
 void DiagramScene::addBox(DiagramBox *newBox, const QPointF &position)

include/diagramscene.h
@@ -28,7 +28,6 @@
 #include "diagrambox.h"
 #include "zone.h"
 #include "openglmatrix.h"
-#include "openglbox.h"
 #include "openglproxy.h"
 
 #include <QGraphicsScene>

include/openglbox.h
@@ -1,24 +1,0 @@
-#ifndef OPENGLBOX_H
-#define OPENGLBOX_H
-
-#include "openglwidget.h"
-#include <QGraphicsScene>
-#include <QGraphicsProxyWidget>
-#include <QObject>
-#include <diagramscene.h>
-
-class OpenGLBox : public QObject
-{
-	Q_OBJECT
-public:
-	OpenGLBox(QGraphicsScene *scene, OpenGLWidget *widget);
-
-public slots:
-	void updateProxy();
-
-private:
-	OpenGLWidget * m_widget;
-	QGraphicsProxyWidget * m_proxy;
-};
-
-#endif // OPENGLBOX_H

include/openglproxy.h
@@ -2,14 +2,17 @@
 #define OPENGLPROXY_H
 
 #include <QGraphicsProxyWidget>
+#include <QGraphicsRectItem>
 #include "openglwidget.h"
 
 class OpenGLProxy : public QGraphicsProxyWidget
 {
 	Q_OBJECT
 public:
-	OpenGLProxy(OpenGLWidget *widget);
+	OpenGLProxy(OpenGLWidget *widget, QGraphicsRectItem *moveBar);
 	~OpenGLProxy();
+
+	QGraphicsRectItem *moveBar() const;
 
 signals:
 	void proxyDestroyed();
@@ -19,6 +22,7 @@ public slots:
 
 private:
 	OpenGLWidget *m_widget;
+	QGraphicsRectItem *m_moveBar;
 };
 
 #endif // OPENGLPROXY_H

include/openglwidget.h
@@ -59,6 +59,7 @@ public:
 	void mousePressEvent(QMouseEvent *event);
 	void mouseMoveEvent(QMouseEvent *event);
 	void keyPressEvent(QKeyEvent* event);
+	void keyReleaseEvent(QKeyEvent* event);
 
 	void rotateView(int x, int y, int z);
 	void translateView(int x, int y, int z);
@@ -99,6 +100,7 @@ protected:
 	int m_current_frame_count;
 	QPoint m_last_pos;
 	QGraphicsProxyWidget *m_proxy;
+	QKeyEvent* m_keyPressed;
 };
 
 #endif // OPENGLWIDGET_H

src/openglbox.cpp
@@ -1,13 +1,0 @@
-#include "openglbox.h"
-
-OpenGLBox::OpenGLBox(QGraphicsScene *scene, OpenGLWidget *widget):m_widget(widget)
-{
-	m_proxy = scene->addWidget(m_widget);
-	connect(widget, SIGNAL(repaint()), this, SLOT(updateProxy()));
-}
-
-void OpenGLBox::updateProxy()
-{
-	qDebug() << "signal received";
-	m_proxy->update();
-}

src/openglproxy.cpp
@@ -1,19 +1,33 @@
 #include "openglproxy.h"
 
-OpenGLProxy::OpenGLProxy(OpenGLWidget *widget):m_widget(widget)
+OpenGLProxy::OpenGLProxy(OpenGLWidget *widget, QGraphicsRectItem *moveBar):m_widget(widget), m_moveBar(moveBar)
 {
 	setWidget(m_widget);
+
+	m_moveBar->setRect(0, 0, m_widget->width(), 20);
+	m_moveBar->setPen(QPen(Qt::black));
+	m_moveBar->setBrush(QBrush(Qt::black));
+	setPos(0, m_moveBar->rect().height());
+	setParentItem(m_moveBar);
+
+	m_moveBar->setFlag(QGraphicsItem::ItemIsMovable, true);
+	m_moveBar->setFlag(QGraphicsItem::ItemIsSelectable, true);
+
 	connect(m_widget, SIGNAL(repaint()), this, SLOT(updateProxy()));
-	setFlag(QGraphicsItem::ItemIsMovable);
-	setFlag(QGraphicsItem::ItemIsSelectable);
-	setFlag(QGraphicsItem::ItemIsFocusable);
 }
 OpenGLProxy::~OpenGLProxy()
 {
 	emit proxyDestroyed();
+	setParentItem(nullptr);
+	delete m_moveBar;
 }
 
 void OpenGLProxy::updateProxy()
 {
 	update();
+}
+
+QGraphicsRectItem *OpenGLProxy::moveBar() const
+{
+	return m_moveBar;
 }

src/openglwidget.cpp
@@ -93,11 +93,15 @@ void OpenGLWidget::mouseMoveEvent(QMouseEvent *event)
 }
 
 void OpenGLWidget::keyPressEvent(QKeyEvent* event) {
-	QKeyEvent* key = static_cast<QKeyEvent*>(event);
-	if(key->key()==Qt::Key_Space)
-	{
+	m_keyPressed = static_cast<QKeyEvent*>(event);
+	if(m_keyPressed->key()==Qt::Key_Space)
 		reinitializeView();
-	}
+}
+
+
+void OpenGLWidget::keyReleaseEvent(QKeyEvent* event)
+{
+	m_keyPressed = nullptr;
 }
 
 void OpenGLWidget::rotateView(int x, int y, int z)

commit 709627fc7ff3846de71217bfc03f1c16ef01c3ea

Fix all bugs caused by previous merge (#44)

include/shaderproxy.h
@@ -20,7 +20,6 @@ class ShaderProxy : public QGraphicsProxyWidget
 	Q_OBJECT
 
 public:
-	ShaderProxy(ShaderWidget *widget, ShaderMoveBar *moveBar, DiagramBox * box);
 	ShaderProxy(ShaderWidget *widget, ShaderMoveBar *moveBar, DiagramBox *box, QMutex *mutex);
 	~ShaderProxy();
 

papyrus.pro
@@ -108,7 +108,9 @@ HEADERS += include/category.h \
     include/shaderscalepolar.h \
     include/visufunctions.h \
     include/shaderscale.h \
-<<<<<<< HEAD
+<<<<<<< HEAD \
+    include/threadshader.h \
+    include/linkvisutobox.h
     include/shadercompass.h \
     include/shaderscaleallplanes.h \
     include/visudialog.h \
@@ -212,7 +214,9 @@ SOURCES += src/category.cpp \
     src/shaderscalepolar.cpp \
     src/visufunctions.cpp \
     src/shaderscale.cpp \
-<<<<<<< HEAD
+<<<<<<< HEAD \
+    src/threadshader.cpp \
+    src/linkvisutobox.cpp
     src/shadercompass.cpp \
     src/shaderscaleallplanes.cpp \
     src/visudialog.cpp \

src/activityvisualizer.cpp
@@ -144,7 +144,6 @@ void ActivityVisualizer::mouseReleaseEvent(QGraphicsSceneMouseEvent *evt)
  */
 void ActivityVisualizer::mouseMoveEvent(QGraphicsSceneMouseEvent *evt)
 {
-	qDebug() << "1";
 	// Define some minimum dimensions so we can't make charts too small
 	qreal newWidth, newHeight;
 
@@ -170,6 +169,7 @@ void ActivityVisualizer::mouseMoveEvent(QGraphicsSceneMouseEvent *evt)
 			;
 	}
 	setPixmap(QPixmap::fromImage(m_image).scaled(m_width, m_height));
+	updateLinkToBox(scenePos());
 
 	QGraphicsPixmapItem::mouseMoveEvent(evt);
 }

src/activityvisualizerbars.cpp
@@ -143,6 +143,7 @@ void ActivityVisualizerBars::mouseMoveEvent(QGraphicsSceneMouseEvent *evt)
 	}
 
 	updatePixmap();
+	updateLinkToBox(scenePos());
 	QGraphicsPixmapItem::mouseMoveEvent(evt);
 }
 

src/activityvisualizerthermal.cpp
@@ -71,6 +71,7 @@ void ActivityVisualizerThermal::mouseMoveEvent(QGraphicsSceneMouseEvent *evt)
 
 //	setPixmap(QPixmap::fromImage(m_image).scaled(m_width, m_height));
 	updatePixmap();
+	updateLinkToBox(scenePos());
 	QGraphicsPixmapItem::mouseMoveEvent(evt);
 }
 

src/diagramscene.cpp
@@ -334,10 +334,7 @@ void DiagramScene::mouseMoveEvent(QGraphicsSceneMouseEvent *evt)
 	if (evt->button() & Qt::LeftButton) {
 		QGraphicsItem *maybeItem = itemAt(mousePos, QTransform());
 		if(!(evt->modifiers() & Qt::ControlModifier) && maybeItem)
-		{
-			qDebug() << "debug";
 			selectedItems().push_back(maybeItem);
-		}
 	}
 
 	// Select all Slots that are visible on the scene (in the view's viewport)
@@ -1361,8 +1358,11 @@ void DiagramScene::onOkBtnClicked(bool)
 		Zone *selectedZone;
 
 		if ((selectedBox = dynamic_cast<DiagramBox *>(item))) {
-			if(selectedBox->getDisplayedProxy() != nullptr)
-				delete selectedBox->getDisplayedProxy();
+			if(selectedBox->displayedProxy() != nullptr)
+			{
+				delete selectedBox->displayedProxy();
+				selectedBox->displayedProxy()->moveBar()->thread()->setRunning(false);  // we don't provide CTRL + Z for deleting visualizer for now
+			}
 			if(selectedBox->activityVisualizer() != nullptr)
 				delete selectedBox->activityVisualizer();
 			propPanel->updateBoxProperties(selectedBox);
@@ -1662,10 +1662,12 @@ void DiagramScene::display2DVisu(VisuType type)
 
 			if(oldProxy != nullptr)
 			{
+				ThreadShader *oldThread = oldProxy->moveBar()->thread();
 				vis->setPos(oldProxy->scenePos().x(), oldProxy->scenePos().y());
 				vis->setWidth(oldProxy->widget()->width());
 				vis->setHeight(oldProxy->widget()->height());
 				delete oldProxy;
+				oldThread->setRunning(false);
 				vis->updatePixmap();
 			}
 			else if(oldVis != nullptr)
@@ -1755,52 +1757,28 @@ void DiagramScene::display3DVisu(VisuType type, QMap<QString, QVariant> paramete
 
 			if (selectedBox->outputType() == MATRIX) {
 				selectedBox->setVisuType(type);
-				// Insert the 3D widget
-				ShaderWidget *widget = createShaderWidget(type, selectedBox->getRows(), selectedBox->getCols(), parameters);
-				ShaderMoveBar *shaderMoveBar = new ShaderMoveBar();
-				ShaderProxy *proxy = new ShaderProxy(widget, shaderMoveBar, selectedBox);
 				selectedBox->fillVisuParameters(parameters);
-				connect(this, SIGNAL(hideShaderWidgets()), proxy, SLOT(hideDisplay()));
-				connect(this, SIGNAL(showShaderWidgets()), proxy, SLOT(showDisplay()));
+				// Insert the 3D widget
+				ThreadShader *thread = new ThreadShader(selectedBox, type, parameters);
+				connect(this, SIGNAL(hideShaderWidgets()), thread->proxy(), SLOT(hideDisplay()));
+				connect(this, SIGNAL(showShaderWidgets()), thread->proxy(), SLOT(showDisplay()));
 
-				addItem(shaderMoveBar);
-
-				if(selectedBox->getDisplayedProxy() != nullptr)
-				{
-					selectedBox->setVisuType(type);
-					selectedBox->fillVisuParameters(parameters);
-					// Insert the 3D widget
-					ThreadShader *thread = new ThreadShader(selectedBox, type, parameters);
-					connect(this, SIGNAL(hideShaderWidgets()), thread->proxy(), SLOT(hideDisplay()));
-					connect(this, SIGNAL(showShaderWidgets()), thread->proxy(), SLOT(showDisplay()));
-					addItem(thread->shaderMoveBar());
-
-					// Create the activity fetcher with the topic name
-					ActivityFetcher *fetcher = nullptr;
-					if (selectedBox->publish()) {
-						fetcher = new ActivityFetcher(selectedBox->topic(), selectedBox);
-					} else {
-						fetcher = new ActivityFetcher(ensureSlashPrefix(mkTopicName(selectedBox->scriptName(),
-						                                                            selectedBox->uuid().toString())),
-						                              selectedBox);
-						m_script->rosSession()->addToHotList(QSet<QUuid>() << selectedBox->uuid());
-					}
-					thread->proxy()->setActivityFetcher(fetcher);
-					connect(fetcher, SIGNAL(newMatrix(QVector<qreal>*)), thread->proxy(), SLOT(updateValues(QVector<qreal>*)));
+				// Create the activity fetcher with the topic name
+				ActivityFetcher *fetcher = nullptr;
+				if (selectedBox->publish()) {
+					fetcher = new ActivityFetcher(selectedBox->topic(), selectedBox);
+				} else {
+					fetcher = new ActivityFetcher(ensureSlashPrefix(mkTopicName(selectedBox->scriptName(),
+					                                                            selectedBox->uuid().toString())),
+					                              selectedBox);
+					m_script->rosSession()->addToHotList(QSet<QUuid>() << selectedBox->uuid());
 				}
+				thread->proxy()->setActivityFetcher(fetcher);
 
-				proxy->setActivityFetcher(fetcher);
-				connect(fetcher, SIGNAL(newMatrix(QVector<qreal>*)), proxy, SLOT(updateValues(QVector<qreal>*)));
+				connect(fetcher, SIGNAL(newMatrix(QVector<qreal>*)), thread->proxy(), SLOT(updateValues(QVector<qreal>*)));
 
-				//draw a link between the box and its visu
-				LinkVisuToBox *linkVisuToBox = new LinkVisuToBox(shaderMoveBar->x()+proxy->widget()->width()/2,
-				                                        shaderMoveBar->y()+proxy->widget()->height()/2,
-				                                        selectedBox->x()+selectedBox->bWidth()/2,
-				                                        selectedBox->y()+selectedBox->bHeight()/2);
-				addItem(linkVisuToBox);
-				proxy->setLinkToBox(linkVisuToBox);
-				shaderMoveBar->setLinkVisuToBox(linkVisuToBox);
-				selectedBox->setLinkVisuToBox(linkVisuToBox);
+				addItem(thread->shaderMoveBar());
+				addItem(thread->proxy()->linkToBox());
 			}
 			else
 				qWarning() << "Output type not supported for 3D visualization";

src/linkvisutobox.cpp
@@ -25,9 +25,9 @@ void LinkVisuToBox::centerDiagramBoxMoved(float x, float y)
 }
 void LinkVisuToBox::centerVisuMoved(float x, float y)
 {
+	qDebug() << m_xVisu << m_yVisu << x << y;
 	m_xVisu = x;
 	m_yVisu = y;
 
 	setLine(QLineF(m_xVisu, m_yVisu, m_xDiagramBox, m_yDiagramBox));
-	//qDebug() << "diagrambox:" << m_xDiagramBox << m_yDiagramBox << "visu:" << m_xVisu << m_yVisu;
 }

src/shaderproxy.cpp
@@ -4,6 +4,7 @@ ShaderProxy::ShaderProxy(ShaderWidget *widget, ShaderMoveBar *moveBar, DiagramBo
     m_widget(widget),
     m_moveBar(moveBar),
     m_box(box),
+    m_linkToBox(nullptr),
     m_mutex(mutex)
 {
 	setWidget(m_widget);
@@ -11,25 +12,7 @@ ShaderProxy::ShaderProxy(ShaderWidget *widget, ShaderMoveBar *moveBar, DiagramBo
 	m_moveBar->setPen(QPen(Qt::black));
 	m_moveBar->setBrush(QBrush(Qt::black));
 	setParentItem(m_moveBar);
-	positionWidget(0, 0);
 
-	m_moveBar->setFlag(QGraphicsItem::ItemIsMovable, true);
-	m_moveBar->setFlag(QGraphicsItem::ItemIsSelectable, true);
-
-	connect(m_widget, SIGNAL(repaint()), this, SLOT(updateProxy()));
-}
-
-ShaderProxy::ShaderProxy(ShaderWidget *widget, ShaderMoveBar *moveBar, DiagramBox * box):
-    m_widget(widget),
-    m_moveBar(moveBar),
-    m_box(box),
-    m_linkToBox(nullptr)
-{
-	setWidget(m_widget);
-	m_moveBar->setRect(0,0,m_widget->width(),m_moveBarHeight);
-	m_moveBar->setPen(QPen(Qt::black));
-	m_moveBar->setBrush(QBrush(Qt::black));
-	setParentItem(m_moveBar);
 	positionWidget(0, 0);
 
 	m_moveBar->setFlag(QGraphicsItem::ItemIsMovable, true);
@@ -47,8 +30,12 @@ ShaderProxy::ShaderProxy(ShaderWidget *widget, ShaderMoveBar *moveBar, DiagramBo
 	m_moveBar->setProxyWidth(m_widget->width());
 	m_moveBar->setProxyHeight(m_widget->height());
 
+	m_moveBar->setFlag(QGraphicsItem::ItemIsMovable, true);
+	m_moveBar->setFlag(QGraphicsItem::ItemIsSelectable, true);
+
 	connect(m_widget, SIGNAL(repaint()), this, SLOT(updateProxy()));
 }
+
 
 ShaderProxy::~ShaderProxy()
 {
@@ -115,17 +102,28 @@ void ShaderProxy::positionWidget(qreal x, qreal y)
 
 void ShaderProxy::resizeWidget(int width, int height)
 {
-	resize(width, height);
-	m_moveBar->setRect(0, 0, width, m_moveBarHeight);
 
 	//Resize the widget if it is too small
-	if(width<m_widget->minWidth())
+	if(width<m_widget->minWidth() && height<m_widget->minHeight())
 	{
-		m_widget->resize(m_widget->minWidth(), m_widget->height());
+		m_widget->resize(m_widget->minWidth(), m_widget->minHeight());
 		m_moveBar->setRect(0, 0, m_widget->minWidth(),m_moveBarHeight);
 	}
-	if(height<m_widget->minHeight())
-		m_widget->resize(m_widget->width(), m_widget->minHeight());
+	else if(width<m_widget->minWidth())
+	{
+		m_widget->resize(m_widget->minWidth(), height);
+		m_moveBar->setRect(0, 0, m_widget->minWidth(),m_moveBarHeight);
+	}
+	else if(height<m_widget->minHeight())
+	{
+		m_widget->resize(width, m_widget->minHeight());
+		m_moveBar->setRect(0, 0, width, m_moveBarHeight);
+	}
+	else
+	{
+		m_widget->resize(width, height);
+		m_moveBar->setRect(0, 0, width, m_moveBarHeight);
+	}
 
 	m_moveBar->setProxyWidth(m_widget->width());
 	m_moveBar->setProxyHeight(m_widget->height());

src/threadshader.cpp
@@ -5,27 +5,40 @@ ThreadShader::ThreadShader(DiagramBox* box, VisuType type, QMap<QString, QVarian
 	m_widget = createShaderWidget(type, box->rows(), box->cols(), parameters);
 	m_shaderMoveBar = new ShaderMoveBar();
 	m_proxy = new ShaderProxy(m_widget, m_shaderMoveBar, box, &m_mutex);
-
 	if(box->displayedProxy() != nullptr)
 	{
 		ShaderProxy *oldProxy = box->displayedProxy();
-		m_proxy->positionWidget(oldProxy->scenePos().x(), oldProxy->scenePos().y());
+		ThreadShader *oldThread = oldProxy->moveBar()->thread();
+
+		m_proxy->positionWidget(oldProxy->scenePos().x(), oldProxy->scenePos().y()-m_proxy->moveBarHeight());
 		m_proxy->resizeWidget(oldProxy->widget()->width(), oldProxy->widget()->height());
+
 		delete oldProxy;
+		oldThread->setRunning(false);
 	}
 	else if(box->isActivityVisuEnabled())
 	{
 		ActivityVisualizer *oldVis = box->activityVisualizer();
-		m_proxy->positionWidget(oldVis->x(), oldVis->y());
+		m_proxy->positionWidget(oldVis->x(), oldVis->y()-m_proxy->moveBarHeight());
 		m_proxy->resizeWidget(oldVis->width(), oldVis->height());
 		delete oldVis;
 		box->setIsActivityVisuEnabled(false);
 	}
 	else
-		m_proxy->positionWidget(box->scenePos().x(), box->scenePos().y() - m_proxy->widget()->height() - 10);
+		m_proxy->positionWidget(box->scenePos().x(), box->scenePos().y() - m_proxy->widget()->height() -m_proxy->moveBarHeight() - 10);
 
 	box->setDisplayedProxy(m_proxy);
 	m_shaderMoveBar->setProxy(m_proxy);
+
+	//draw a link between the box and its visu
+	LinkVisuToBox *linkVisuToBox = new LinkVisuToBox(m_shaderMoveBar->x()+m_proxy->widget()->width()/2,
+	                                        m_shaderMoveBar->y()+m_proxy->widget()->height()/2,
+	                                        box->x()+box->bWidth()/2,
+	                                        box->y()+box->bHeight()/2);
+	m_proxy->setLinkToBox(linkVisuToBox);
+	m_shaderMoveBar->setLinkVisuToBox(linkVisuToBox);
+	box->setLinkVisuToBox(linkVisuToBox);
+
 	m_shaderMoveBar->setThread(this);
 
 	start();

src/xmlscriptreader.cpp
@@ -510,7 +510,6 @@ void XmlScriptReader::readFunction(std::map<QUuid, DiagramBox *> *allBoxes,
 			QObject::connect(dynamic_cast<DiagramScene *>(b->scene()), SIGNAL(hideShaderWidgets()), thread->proxy(), SLOT(hideDisplay()));
 			QObject::connect(dynamic_cast<DiagramScene *>(b->scene()), SIGNAL(showShaderWidgets()), thread->proxy(), SLOT(showDisplay()));
 
-			b->scene()->addItem(thread->shaderMoveBar());
 			b->setDisplayedProxy(thread->proxy());
 
 			// Create the activity fetcher with the topic name
@@ -523,19 +522,12 @@ void XmlScriptReader::readFunction(std::map<QUuid, DiagramBox *> *allBoxes,
 				                              b);
 				m_script->rosSession()->addToHotList(QSet<QUuid>() << b->uuid());
 			}
-      thread->proxy()->setActivityFetcher(fetcher);
-      QObject::connect(fetcher, SIGNAL(newMatrix(QVector<qreal>*)), thread->proxy(), SLOT(updateValues(QVector<qreal>*)));
-      thread->proxy()->setVisible(visuVisible);
+			thread->proxy()->setActivityFetcher(fetcher);
+			QObject::connect(fetcher, SIGNAL(newMatrix(QVector<qreal>*)), thread->proxy(), SLOT(updateValues(QVector<qreal>*)));
+			thread->proxy()->setVisible(visuVisible);
 
-			//draw a link between the box and its visu
-			LinkVisuToBox *linkVisuToBox = new LinkVisuToBox(thread->shaderMoveBar()->x()+thread->proxy()->widget()->width()/2,
-			                                        thread->shaderMoveBar()->y()+thread->proxy()->widget()->height()/2,
-			                                        b->x()+b->bWidth()/2,
-			                                        b->y()+b->bHeight()/2);
-			b->scene()->addItem(linkVisuToBox);
-			thread->proxy()->setLinkToBox(linkVisuToBox);
-			thread->shaderMoveBar()->setLinkVisuToBox(linkVisuToBox);
-			b->setLinkVisuToBox(linkVisuToBox);
+			b->scene()->addItem(thread->shaderMoveBar());
+			b->scene()->addItem(thread->proxy()->linkToBox());
 		}
 		else
 			qWarning() << "Unknown visualization type";